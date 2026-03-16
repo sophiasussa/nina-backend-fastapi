aef96b1f9aa25ff2544c37320d748899fbef9229
sophiasussa <sophiasussa@outlook.com>
2026-03-16 19:28:39 -0300

    ci: add initial GitHub Actions pipeline

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from app.modules.auth.presentation.routes.auth_routes import router as example_router
 
-app = FastAPI()
+app = FastAPI(
+    title="Example API",
+    version="0.1.0",
+)
+
+app.include_router(example_router)
+
 
 @app.get("/")
-def root():
+def health_check():
     return {"status": "ok"}
